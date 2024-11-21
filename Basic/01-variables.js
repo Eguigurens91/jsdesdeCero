d623d121166cb9e9c49f57b210d88e4041d7d2e5
Mariel <eguigurensmariell@gmail.com>
2024-11-21 11:45:23 -0600

    Varibles y dataTypes

---
+++ Basic/01-variables.js
@@ -0,0 +1,17 @@
+// variable tipo var, ya no es muy usada se usa solo en casos muy especificos
+var helloWordl = "Hola Mundo JS!"
+console.log(helloWordl)
+
+helloWordl = "Hola de nuevo!"
+console.log(helloWordl)
+
+// let
+let helloWordl2 = "Hola Mundo JS!"
+console.log(helloWordl2)
+
+helloWordl2 = "Hola de nuevo!"
+console.log(helloWordl2)
+
+// const
+const helloWordl3 = "Hola Mundo JS!"
+console.log(helloWordl3)
